d5e4b9f1b85eb03471ff330c0bdc769554fbace7
JavierM <144504968+jav0314@users.noreply.github.com>
2024-03-16 18:51:34 -0500

    show password and styles changes

--- src/JS/main.js
+++ src/JS/main.js
@@ -1,8 +1,33 @@
-export function PasswordVisibility() {
-  var passwordInput = document.getElementById("password");
-  if (passwordInput.type === "password") {
-    passwordInput.type = "text";
-  } else {
-    passwordInput.type = "password";
-  }
-}
+const handlePasswordVisibility = () => {
+  const showPasswordCTA = document.getElementById("showPassword");
+  const passwordCTA = document.getElementById("password");
+  if (!showPasswordCTA || !passwordCTA) throw new Error();
+
+  showPasswordCTA.addEventListener("change", (event) => {
+    if (event.target.checked) {
+      passwordCTA.type = "text";
+      return;
+    }
+    passwordCTA.type = "password";
+  });
+};
+
+document.addEventListener("DOMContentLoaded", () => {
+  handlePasswordVisibility();
+});
+
+// const dataLogIn = () => {
+//   const user = "admin";
+//   const pass = "1234";
+//   const nameUser = document.getElementById("username").value;
+//   const passUser = document.getElementById("password").value;
+//   if (nameUser === user || passUser === pass) {
+//     alert("Usuario o contraseña incorrectos");
+//   } else {
+//     alert("Bienvenido");
+//   }
+// };
+
+// document.addEventListener("DOMContentLoaded", () => {
+//   dataLogIn();
+// });
