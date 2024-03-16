365e038cccb2224c93f47d5b06dcc2f837a0ba04
Julio Salas <84051776+julio-salas03@users.noreply.github.com>
2024-03-16 11:08:37 -0500

    fix show password functionality

--- src/JS/main.js
+++ src/JS/main.js
@@ -1,8 +1,20 @@
-export function PasswordVisibility() {
-  var passwordInput = document.getElementById("password");
-  if (passwordInput.type === "password") {
-    passwordInput.type = "text";
-  } else {
-    passwordInput.type = "password";
-  }
+const handlePasswordVisibility = () => {
+  const showPasswordCTA = document.getElementById("showPassword")
+  const passwordCTA = document.getElementById("password")
+  /**
+   * Add a message to the error if you want
+   */
+  if (!showPasswordCTA || !passwordCTA) throw new Error()
+
+  showPasswordCTA.addEventListener("change", (event) => {
+    if (event.target.checked) {
+      passwordCTA.type = "text"
+      return
+    }
+    passwordCTA.type = "password"
+  })
 }
+
+document.addEventListener("DOMContentLoaded", () => {
+  handlePasswordVisibility()
+})
