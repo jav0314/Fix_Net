e145eec1608bea3ece8ce2a6a35a8fccc5718733
JavierM <144504968+jav0314@users.noreply.github.com>
2024-03-16 10:54:41 -0500

    files structure

---
+++ src/JS/main.js
@@ -0,0 +1,8 @@
+export function PasswordVisibility() {
+  var passwordInput = document.getElementById("password");
+  if (passwordInput.type === "password") {
+    passwordInput.type = "text";
+  } else {
+    passwordInput.type = "password";
+  }
+}
